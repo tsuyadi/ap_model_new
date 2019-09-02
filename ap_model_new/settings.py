bdddc7a438c363fd2079539443ae6faaf324d68f
tsuyadi <tsuyadi@gmail.com>
2019-09-02 15:51:55 +0700

    static file

--- ap_model_new/settings.py
+++ ap_model_new/settings.py
@@ -15,6 +15,10 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+DIR_NAME = os.path.dirname(__file__)
+
+STATICFILES_DIRS = [str(BASE_DIR + '/static'), ]
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -136,3 +140,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(DIR_NAME, 'static_root')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(DIR_NAME, 'media_root')
+
+# STATICFILES_DIRS = (
+#     os.path.join(DIR_NAME, 'static'),
+#     os.path.join(DIR_NAME, 'media'),
+# )
